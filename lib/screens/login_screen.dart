e5d43b80345678c4073e33027ca4ef4c4dc08443
sonsiwawong <sonsiwawong@nagasdigital.co.th>
2024-10-01 10:23:40 +0700

    feat(login): add tap to dismiss keyboard functionality
    Add a GestureDetector to the Scaffold to dismiss the keyboard when the user taps outside of a text input field.
    
    This enhances the user experience by allowing them to easily dismiss the keyboard without having to tap on a specific button or area.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -18,89 +18,89 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                const Text(
-                  'เข้าสู่ระบบ',
-                  style: TextStyle(
-                    fontSize: 36.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 36.0),
-                TextFormField(
-                  controller: _emailController,
-                  autofocus: true,
-                  decoration: const InputDecoration(
-                    labelText: 'Username',
-                    labelStyle: TextStyle(
-                      color: Colors.black,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  const Text(
+                    'เข้าสู่ระบบ',
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  textInputAction: TextInputAction.next,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'โปรดกรอก username ของคุณ';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16.0),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    labelText: 'รหัสผ่าน',
-                    labelStyle: const TextStyle(
-                      color: Colors.black,
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                  const SizedBox(height: 36.0),
+                  TextFormField(
+                    controller: _emailController,
+                    autofocus: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                      labelStyle: TextStyle(
                         color: Colors.black,
                       ),
-                      onPressed: () {
-                        setState(() {
-                          isPasswordVisible = !isPasswordVisible;
-                        });
-                      },
                     ),
+                    textInputAction: TextInputAction.next,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'โปรดกรอก username ของคุณ';
+                      }
+                      return null;
+                    },
                   ),
-                  obscureText: !isPasswordVisible,
-                  focusNode: _passwordFocusNode,
-                  textInputAction: TextInputAction.done,
-                  onFieldSubmitted: (_) async {
-                    if (_formKey.currentState!.validate()) {
-                      await _authController.login(
-                        _emailController.text,
-                        _passwordController.text,
-                      );
-                    } else {
-                      FocusScope.of(context).requestFocus(_passwordFocusNode);
-                    }
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'โปรดกรอกรหัสผ่านของคุณ';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 24),
-                Obx(() {
-                  if (_authController.isLoading.value) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-
-                  return PrimaryButton(
+                  const SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'รหัสผ่าน',
+                      labelStyle: const TextStyle(
+                        color: Colors.black,
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            isPasswordVisible = !isPasswordVisible;
+                          });
+                        },
+                      ),
+                    ),
+                    obscureText: !isPasswordVisible,
+                    focusNode: _passwordFocusNode,
+                    textInputAction: TextInputAction.done,
+                    onFieldSubmitted: (_) async {
+                      if (_formKey.currentState!.validate()) {
+                        await _authController.login(
+                          _emailController.text,
+                          _passwordController.text,
+                        );
+                      } else {
+                        FocusScope.of(context).requestFocus(_passwordFocusNode);
+                      }
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'โปรดกรอกรหัสผ่านของคุณ';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 24),
+                  PrimaryButton(
                     text: 'เข้าสู่ระบบ',
+                    isLoading: _authController.isLoading,
                     onPressed: () async {
                       if (_formKey.currentState!.validate()) {
                         await _authController.login(
@@ -109,24 +109,24 @@ class _LoginScreenState extends State<LoginScreen> {
                         );
                       }
                     },
-                  );
-                }),
-                Obx(() {
-                  if (_authController.errorMessage.value.isNotEmpty) {
-                    return Column(
-                      children: [
-                        Text(
-                          _authController.errorMessage.value,
-                          style: const TextStyle(color: Colors.red),
-                        ),
-                        const SizedBox(height: 10),
-                      ],
-                    );
-                  }
+                  ),
+                  Obx(() {
+                    if (_authController.errorMessage.value.isNotEmpty) {
+                      return Column(
+                        children: [
+                          Text(
+                            _authController.errorMessage.value,
+                            style: const TextStyle(color: Colors.red),
+                          ),
+                          const SizedBox(height: 10),
+                        ],
+                      );
+                    }
 
-                  return Container();
-                })
-              ],
+                    return Container();
+                  })
+                ],
+              ),
             ),
           ),
         ),
