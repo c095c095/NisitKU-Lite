4b4967555f7a11ce6ce3911a9930e36721849485
sonsiwawong <sonsiwawong@nagasdigital.co.th>
2024-10-01 10:23:00 +0700

    feat: Add loading state to primary button
    This change introduces a loading state to the primary button, allowing it to display a circular progress indicator while the button is disabled.
    
    The loading state is controlled by an `isLoading` RxBool, which can be set to `true` or `false` to indicate whether the button should be in a loading state. When the `isLoading` value is `true`, the button will be disabled and the progress indicator will be displayed. When the `isLoading` value is `false`, the button will be enabled and the progress indicator will be hidden.
    
    This change improves the user experience by providing visual feedback to the user when the button is disabled due to a long-running operation.

--- lib/components/primary_button.dart
+++ lib/components/primary_button.dart
@@ -1,38 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class PrimaryButton extends StatelessWidget {
   final String text;
-  final VoidCallback onPressed;
+  final VoidCallback? onPressed;
+  final RxBool isLoading;
 
-  const PrimaryButton({
+  PrimaryButton({
     super.key,
     required this.text,
     required this.onPressed,
-  });
+    RxBool? isLoading,
+  }) : isLoading = isLoading ?? false.obs;
 
   @override
   Widget build(BuildContext context) {
-    return FilledButton(
-      style: FilledButton.styleFrom(
-        backgroundColor: Colors.black,
-        minimumSize: const Size(double.infinity, 48.0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-      ),
-      onPressed: onPressed,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 16.0,
-        ),
-        child: Text(
-          text,
-          style: const TextStyle(
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
+    return Obx(() {
+      return FilledButton(
+        style: FilledButton.styleFrom(
+          backgroundColor: Colors.black,
+          minimumSize: const Size(double.infinity, 48.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
           ),
         ),
-      ),
-    );
+        onPressed: isLoading.value ? null : onPressed,
+        child: isLoading.value
+            ? const SizedBox(
+                width: 24.0,
+                height: 24.0,
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                  strokeWidth: 2.0,
+                ),
+              )
+            : Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+      );
+    });
   }
 }
